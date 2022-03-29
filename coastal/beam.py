f974fa080e0b3ac24c47c7f3bf13a89197ba775b
Júlio Caineta <julio.caineta@pitt.edu>
2022-03-28 21:46:05 -0400

    pep style, small typo

--- coastal/beam.py
+++ coastal/beam.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Tools for meam manipulation.
+Tools for beam manipulation.
 
 """
 from sliderule import icesat2
 
 
-def reduce_dataframe(gdf, RGT=None, GT=None, track=None, pair=None, cycle=None, beam='', crs=4326):
+def reduce_dataframe(gdf, rgt=None, gt=None, track=None, pair=None, cycle=None, beam='', crs=4326):
     """
     Reduce GeoDataFrame to plot a single beam.
     This function is goint to subset the geodataframe. Since we had a span of
@@ -18,13 +18,13 @@ def reduce_dataframe(gdf, RGT=None, GT=None, track=None, pair=None, cycle=None, 
     # convert coordinate reference system
     D3 = gdf.to_crs(crs)
     # reduce to reference ground track
-    if RGT is not None:
-        D3 = D3[D3["rgt"] == RGT]
+    if rgt is not None:
+        D3 = D3[D3["rgt"] == rgt]
     # reduce to ground track (gt[123][lr]), track ([123]), or pair (l=0, r=1)
     gtlookup = {icesat2.GT1L: 1, icesat2.GT1R: 1, icesat2.GT2L: 2, icesat2.GT2R: 2, icesat2.GT3L: 3, icesat2.GT3R: 3}
     pairlookup = {icesat2.GT1L: 0, icesat2.GT1R: 1, icesat2.GT2L: 0, icesat2.GT2R: 1, icesat2.GT3L: 0, icesat2.GT3R: 1}
-    if GT is not None:
-        D3 = D3[(D3["track"] == gtlookup[GT]) & (D3["pair"] == pairlookup[GT])]
+    if gt is not None:
+        D3 = D3[(D3["track"] == gtlookup[gt]) & (D3["pair"] == pairlookup[gt])]
     if track is not None:
         D3 = D3[D3["track"] == track]
     if pair is not None:
@@ -32,9 +32,9 @@ def reduce_dataframe(gdf, RGT=None, GT=None, track=None, pair=None, cycle=None, 
     # reduce to weak or strong beams
     # tested on cycle 11, where the strong beam in the pair matches the spacecraft orientation.
     # Need to check on other cycles
-    if (beam == 'strong'):
+    if beam == 'strong':
         D3 = D3[D3['sc_orient'] == D3['pair']]
-    elif (beam == 'weak'):
+    elif beam == 'weak':
         D3 = D3[D3['sc_orient'] != D3['pair']]
     # reduce to cycle
     if cycle is not None:
